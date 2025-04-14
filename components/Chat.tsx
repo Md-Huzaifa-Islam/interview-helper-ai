06d1a4afbc57312531ce216d6947f0a19e991458
Md-Huzaifa-Islam <HuzaifaIslamRakib@gmail.com>
2025-04-14 16:37:59 +0600

    made chat component which handle frontside

---
+++ components/Chat.tsx
@@ -0,0 +1,26 @@
+"use client";
+import React from "react";
+import { useChat } from "@ai-sdk/react";
+export default function Chat() {
+  const { messages, input, handleInputChange, handleSubmit } = useChat();
+
+  return (
+    <div className="mx-auto flex w-full max-w-md flex-col self-stretch py-24">
+      {messages.map((m: object) => (
+        <div key={m.id} className="whitespace-pre-wrap">
+          {m.role == "user" ? "User: " : "Ai: "}
+          {m.content}
+        </div>
+      ))}
+
+      <form onSubmit={handleSubmit}>
+        <input
+          className="fixed bottom-0 mb-8 w-full max-w-md rounded border border-gray-300 p-2 shadow-xl"
+          placeholder="Say something"
+          value={input}
+          onChange={handleInputChange}
+        />
+      </form>
+    </div>
+  );
+}
